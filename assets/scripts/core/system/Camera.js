f94dc36e8a4d2338ffadd5f326c6158ddc6c6db1
Pbin <jie6382@gmail.com>
2017-03-18 19:26:16 +0800

    镜头增加offset参数，用于保证边界判断正确

--- assets/scripts/core/system/Camera.js
+++ assets/scripts/core/system/Camera.js
@@ -1,9 +1,14 @@
 cc.Class({
     extends: cc.Component,
 
-    properties: {},
+    properties: {
+        horizontalOffset: 0,
+        verticalOffset: 0,
+    },
 
     onLoad: function() {
+        //offset补偿人物锚点与画面中心的距离
+
         this.gameNode = cc.find('Game');
         this.game = this.gameNode.getComponent("Game");
         this.scene = cc.find('Canvas').getComponent("Scene");
@@ -23,8 +28,8 @@ cc.Class({
         var target = this.scene.getActorTarget(this.game.playerId);
         var pos = target.getPos();
 
-        pos.x = cc.clampf(-pos.x, this.minX, this.maxX);
-        pos.y = cc.clampf(-pos.y, this.minY, this.maxY);
+        pos.x = cc.clampf(-(pos.x + this.horizontalOffset), this.minX, this.maxX);
+        pos.y = cc.clampf(-(pos.y + this.verticalOffset), this.minY, this.maxY);
 
         this.node.setPosition(pos);
     },
